cca9c16dc5c58f063a6e8b5b7369005aac783765
Marcial Rosales <mrosales@pivotal.io>
2022-08-24 16:57:43 +0100

    Use official uaa fully configured via uaa.yml

--- deps/rabbitmq_management/selenium/test/utils.js
+++ deps/rabbitmq_management/selenium/test/utils.js
@@ -3,15 +3,22 @@ require("chromedriver");
 
 var baseUrl = process.env.RABBITMQ_URL;
 var runLocal = process.env.RUN_LOCAL;
+var seleniumUrl = process.env.SELENIUM_URL;
+if (!process.env.RUN_LOCAL) {
+  runLocal = true;
+}
 if (!process.env.RABBITMQ_URL) {
-  baseUrl = "http://local-rabbitmq:15672";
+  baseUrl = "http://localhost:15672";
+}
+if (!process.env.SELENIUM_URL) {
+  seleniumUrl = "http://selenium:4444/wd/hub";
 }
 
 module.exports = {
   buildDriver: (caps) => {
     builder = new Builder().forBrowser('chrome');
     if (!runLocal) {
-      builder = builder.usingServer("http://selenium:4444/wd/hub")
+      builder = builder.usingServer(seleniumUrl)
     }
     driver = builder.build();
     return driver;
